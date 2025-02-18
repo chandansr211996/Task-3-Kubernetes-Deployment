095d26a9f8bc255194343ff8311b45539277a0f9
chandan S R <54269632+chandansr211996@users.noreply.github.com>
2025-02-18 16:18:04 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+python
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from the Python app!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
